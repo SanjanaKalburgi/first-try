eace216dc3516418df85c485fbfb3f033647aa88
SanjanaKalburgi <89793160+SanjanaKalburgi@users.noreply.github.com>
2021-08-30 20:37:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+box=createSprite(200,200,10,10)
+}
+
+function draw() 
+{
+  background(30);
+if (keyDown("left")){
+  box.x=box.x-2
+}
+if (keyDown("right")){
+  box.x=box.x+2
+}
+drawSprites();
+}
+
+
+
+
